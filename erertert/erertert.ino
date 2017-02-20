25c1239d15a602116f781d683cfffbb71a97699d
thgus1652 <thgus1652222@gmail.com>
2017-02-20 11:33:03 +0900

    i fixed file(1)(2)
    i fixed file(1)(2)

--- erertert/erertert.ino
+++ erertert/erertert.ino
@@ -3,10 +3,12 @@
 void setup()
 {
 // Add your initialization code hereergergergergergergergergerergergerergergergergg
+  //i fixed here (1)
 }
 
 // The loop function is called in an endless loopsefsef
 void loop()
 {
 //Add your repeated code hereefsefsefeffgrgrgdrgdrgwefwefwefwefrgergergergergergergergergr3r23r
+  //i fixed here (2)
 }
